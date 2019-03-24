50f431c878703faecd545318836bc321baca2852
MRodrick <32981608+MRodrick@users.noreply.github.com>
2019-03-24 17:00:31 +0000

    Changes to selection wheel and wheel code

--- Assets/Scripts/Selection_Wheel.cs
+++ Assets/Scripts/Selection_Wheel.cs
@@ -6,16 +6,24 @@ public class Selection_Wheel : Player_New
 {
     public GameObject wheel;
     public GameObject Torch;
+    public GameObject House;
+    public GameObject Tent;
+    public GameObject Wall;
+    public GameObject Player;
     // Start is called before the first frame update
+    Vector3 playerPos;
+
     void Start()
     {
         wheel.SetActive(false);
-        
+        playerPos = Player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerPos = Player.transform.position;
+
         if (Input.GetKeyDown(KeyCode.I)) {
             wheel.SetActive(true);
         }
@@ -25,10 +33,22 @@ public class Selection_Wheel : Player_New
         }
     }
     public void opt1() {
-
-
-        Instantiate(Torch, new Vector3(-1.31f,1.24f,-3), Quaternion.identity);
-
-
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y - .3f, playerPos.z);
+        Instantiate(Torch, pos, Quaternion.identity);
+    }
+    public void opt2()
+    {
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+        Instantiate(House, pos, Quaternion.identity);
+    }
+    public void opt3()
+    {
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+        Instantiate(Wall, pos, Quaternion.identity);
+    }
+    public void opt4()
+    {
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+        Instantiate(Tent, pos, Quaternion.identity);
     }
 }
