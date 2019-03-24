31885809fd6129503a47d5bb20c7ac7abcd30361
MRodrick <32981608+MRodrick@users.noreply.github.com>
2019-03-24 20:46:35 +0000

    Added sounds

--- Assets/Scripts/TextFade.cs
+++ Assets/Scripts/TextFade.cs
@@ -10,20 +10,20 @@ public class TextFade : MonoBehaviour
     public Text m_Image;
     //Use this to tell if the toggle returns true or false
     bool m_Fading;
-
+    float i;
     void Update()
     {
         //If the toggle returns true, fade in the Image
-       /* if (m_Fading == true)
+        if (m_Fading == true)
         {
             //Fully fade in Image (1) with the duration of 2
-            m_Image.CrossFadeAlpha(1, 2.0f, false);
+            m_Image.CrossFadeAlpha(1, 3.0f, false);
         }
         //If the toggle is false, fade out to nothing (0) the Image with a duration of 2
         if (m_Fading == false)
         {
-            m_Image.CrossFadeAlpha(0, 2.0f, false);
-        }*/
+            m_Image.CrossFadeAlpha(0, 3, false);
+        }
 
     }
 
