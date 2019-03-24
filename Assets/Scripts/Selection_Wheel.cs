6bd76547ca8fec472479b3760480836bbc3635d0
MRodrick <32981608+MRodrick@users.noreply.github.com>
2019-03-24 18:41:12 +0000

    Added trigger to wall

--- Assets/Scripts/Selection_Wheel.cs
+++ Assets/Scripts/Selection_Wheel.cs
@@ -18,6 +18,7 @@ public class Selection_Wheel : Player_New
         wheel.SetActive(false);
 
         playerPos = Player.transform.position;
+        
     }
 
     // Update is called once per frame
@@ -40,17 +41,17 @@ public class Selection_Wheel : Player_New
     }
     public void opt2()
     {
-        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z+0.1f);
         Instantiate(House, pos, Quaternion.identity);
     }
     public void opt3()
     {
-        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z + 0.1f);
         Instantiate(Wall, pos, Quaternion.identity);
     }
     public void opt4()
     {
-        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z);
+        Vector3 pos = new Vector3(playerPos.x, playerPos.y, playerPos.z + 0.1f);
         Instantiate(Tent, pos, Quaternion.identity);
     }
 }
