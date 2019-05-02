fafebd6ab4674f40175621c5d1fad5e4ce0f3817
MRodrick <32981608+MRodrick@users.noreply.github.com>
2019-05-02 11:47:31 +0100

    Added Tutorial

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public UnityEngine.UI.Text text;
+    private void Start()
+    {
+        text.gameObject.active = false;
+    }
+    public int i;
+    public void mainMenuSelection() {
+        if (i == 1)
+        {
+            LoadScene("GameScene1");
+        }
+        else if (i == 2) {
+         //   ShowTutorial();
+        }
+
+
+    }
+
     public void LoadScene(string GameScene1)
     {
         SceneManager.LoadScene(GameScene1);
+    }
+    public void ShowTutorial(UnityEngine.UI.Text text) {
+        text.gameObject.active = !text.gameObject.active;
     }
 }
 
