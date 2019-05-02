fcd7dc9fd24c87c02df3451fbf5b2904b362da15
MRodrick <32981608+MRodrick@users.noreply.github.com>
2019-05-02 13:16:46 +0100

    Working main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,21 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
-    public UnityEngine.UI.Text text;
+    public Text text;
+    public Button play;
+    public Button tutorial;
+    public Button controls;
+    public Button quit;
+    public Button back;
+    public int i;
+
     private void Start()
     {
-        text.gameObject.active = false;
+        text.gameObject.SetActive(false);
+        back.gameObject.SetActive(false);
     }
-    public int i;
+    
     public void mainMenuSelection() {
         if (i == 1)
         {
             LoadScene("GameScene1");
         }
-        else if (i == 2) {
-         //   ShowTutorial();
+        else if (i == 2)
+        {
+            ShowTutorial();
+        }
+        else if (i == 3)
+        {
+
+        }
+        else if (i == 4) {
+
         }
 
 
@@ -26,8 +43,31 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(GameScene1);
     }
-    public void ShowTutorial(UnityEngine.UI.Text text) {
-        text.gameObject.active = !text.gameObject.active;
+
+
+    public void ShowTutorial() {
+        // play.gameObject.active = false;
+        play.gameObject.active = false;
+        tutorial.gameObject.active = false;
+        controls.gameObject.active = false;
+        quit.gameObject.active = false;
+        back.gameObject.active = true;
+        text.gameObject.active = true;
     }
+
+
+    public void BackButton() {
+        play.gameObject.active = true;
+        tutorial.gameObject.active = true;
+        controls.gameObject.active = true;
+        quit.gameObject.active = true;
+        back.gameObject.active = false;
+        text.gameObject.active = false;
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
 }
 
