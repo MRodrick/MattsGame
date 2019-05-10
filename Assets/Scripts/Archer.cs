cefd372753dd8d31e0f4b8cea9e365a072c41c91
MRodrick <32981608+MRodrick@users.noreply.github.com>
2019-05-10 20:44:42 +0100

    Added arrows

---
+++ Assets/Scripts/Archer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+
+    [Tooltip("The angle of the forward of the view cone. 0 is forward of the sprite, 90 is up, 180 behind etc.")]
+    [Range(0.0f, 360.0f)]
+    public float viewDirection = 0.0f;
+    [Range(0.0f, 360.0f)]
+    public float viewFov;
+    public float viewDistance;
+    [Tooltip("Time in seconds without the target in the view cone before the target is considered lost from sight")]
+    public float timeBeforeTargetLost = 3.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
